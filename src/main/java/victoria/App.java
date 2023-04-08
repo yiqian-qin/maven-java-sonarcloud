1b2bbf1760cf7ce119823e20650026e26857d73d
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:42:37 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,20 +2,35 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int canBeTypedWords(String text, String brokenLetters) {
-        int count = 0;
-        String[] words = text.split(" ");
-        for(String word : words){
-            boolean flag = true;
-            for(int i = 0; i < brokenLetters.length(); i++){
-                if(word.contains(brokenLetters.charAt(i) + "")){
-                    flag = false;
-                    break;
-                }
+    public int[] sortEvenOdd(int[] nums) {
+        int[] odd = new int[nums.length];
+        int[] even = new int[nums.length];
+        int oddCount = 0;
+        int evenCount = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i % 2 == 0){
+                even[evenCount] = nums[i];
+                evenCount++;
+            }else{
+                odd[oddCount] = nums[i];
+                oddCount++;
             }
-            if(flag) count++;
         }
-        return count;
+        Arrays.sort(odd, 0, oddCount);
+        Arrays.sort(even, 0, evenCount);
+        int[] result = new int[nums.length];
+        int oddIndex = oddCount - 1;
+        int evenIndex = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i % 2 == 0){
+                result[i] = even[evenIndex];
+                evenIndex++;
+            }else{
+                result[i] = odd[oddIndex];
+                oddIndex--;
+            }
+        }
+        return result;
 
     }
 }
