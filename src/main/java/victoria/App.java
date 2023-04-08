43cd5e2420282860587aca78b93b845816d2d26e
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 16:34:02 -0400

    add maven app

---
+++ src/main/java/victoria/App.java
@@ -0,0 +1,13 @@
+package victoria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
