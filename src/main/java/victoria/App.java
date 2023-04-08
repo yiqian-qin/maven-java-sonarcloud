a66b7152e73d96dc07246fc27f4011dc9bce5fe7
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 18:04:29 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,13 +2,26 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int countDigitOne(int n) {
-        int count = 0;
-        for(long i = 1; i <= n; i *= 10){
-            long divider = i * 10;
-            count += (n / divider) * i + Math.min(Math.max(n % divider - i + 1, 0), i);
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = capital[i];
+            arr[i][1] = profits[i];
         }
-        return count;
-        
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        int i = 0;
+        while (k-- > 0) {
+            while (i < n && arr[i][0] <= w) {
+                pq.offer(arr[i++][1]);
+            }
+            if (!pq.isEmpty()) {
+                w += pq.poll();
+            } else {
+                break;
+            }
+        }
+        return w;
     }
 }
