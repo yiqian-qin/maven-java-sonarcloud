3e6b5928f92bf1ae9debdb3b4ebdb594f412a00c
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 18:08:46 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,53 +2,22 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public String countOfAtoms(String formula) {
-        int n = formula.length();
-        Stack<Map<String, Integer>> stack = new Stack<>();
-        stack.push(new HashMap<>());
-        for (int i = 0; i < n; i++) {
-            char c = formula.charAt(i);
-            if (c == '(') {
-                stack.push(new HashMap<>());
-            } else if (c == ')') {
-                Map<String, Integer> top = stack.pop();
-                int iStart = ++i;
-                while (i < n && Character.isDigit(formula.charAt(i))) {
-                    i++;
-                }
-                int multiplier = iStart < i ? Integer.parseInt(formula.substring(iStart, i)) : 1;
-                for (String key : top.keySet()) {
-                    int v = top.get(key);
-                    stack.peek().put(key, stack.peek().getOrDefault(key, 0) + v * multiplier);
-                }
-                i--;
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        List<Interval> res = new ArrayList<>();
+        List<Interval> all = new ArrayList<>();
+        for (List<Interval> list : schedule) {
+            all.addAll(list);
+        }
+        Collections.sort(all, (a, b) -> a.start - b.start);
+        int end = all.get(0).end;
+        for (int i = 1; i < all.size(); i++) {
+            if (all.get(i).start > end) {
+                res.add(new Interval(end, all.get(i).start));
+                end = all.get(i).end;
             } else {
-                int iStart = i++;
-                while (i < n && Character.isLowerCase(formula.charAt(i))) {
-                    i++;
-                }
-                String name = formula.substring(iStart, i);
-                iStart = i;
-                while (i < n && Character.isDigit(formula.charAt(i))) {
-                    i++;
-                }
-                int count = iStart < i ? Integer.parseInt(formula.substring(iStart, i)) : 1;
-                stack.peek().put(name, stack.peek().getOrDefault(name, 0) + count);
-                i--;
+                end = Math.max(end, all.get(i).end);
             }
         }
-        Map<String, Integer> map = stack.pop();
-        List<String> list = new ArrayList<>(map.keySet());
-        Collections.sort(list);
-        StringBuilder sb = new StringBuilder();
-        for (String key : list) {
-            sb.append(key);
-            int v = map.get(key);
-            if (v > 1) {
-                sb.append(v);
-            }
-        }
-        return sb.toString();
-        
+        return res;
     }
 }
