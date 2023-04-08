7c2b55730c21fec68e97e8f9833e01178790e6c2
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 16:41:55 -0400

    update code file

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -1,13 +1,25 @@
-package victoria;
+import java.util.*;
+import java.util.Arrays;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+class Solution {
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a,b) -> a[1] - b[1]); // sort courses by last day
+
+        PriorityQueue<Integer> takenCourses = new PriorityQueue<>((a, b) -> b - a); // max heap to keep track of taken courses
+        int currentDay = 0; // current day we start a course
+
+        for (int[] course : courses) {
+            int duration = course[0], lastDay = course[1];
+            if (currentDay + duration <= lastDay) {
+                takenCourses.offer(duration);
+                currentDay += duration;
+            } else if (!takenCourses.isEmpty() && takenCourses.peek() > duration) {
+                // replace a longer course with this one to reduce total days taken
+                currentDay += duration - takenCourses.poll();
+                takenCourses.offer(duration);
+            }
+        }
+
+        return takenCourses.size();
     }
 }
