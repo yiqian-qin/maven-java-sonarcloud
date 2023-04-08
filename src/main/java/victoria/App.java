f1226022f8b0777ab1f72b1ad2b4052c55677725
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:34:01 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,13 +2,16 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int[] constructRectangle(int area) {
-        int[] ans = new int[2];
-        int w = (int)Math.sqrt(area);
-        while(area % w != 0) w--;
-        ans[0] = area / w;
-        ans[1] = w;
-        return ans;
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int total = 0;
+        for(int i = 0; i < timeSeries.length; i++){
+            if(i == timeSeries.length-1){
+                total += duration;
+            }else{
+                total += Math.min(timeSeries[i+1] - timeSeries[i], duration);
+            }
+        }
+        return total;
 
     }
 }
