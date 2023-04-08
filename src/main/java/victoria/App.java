3abe667052e02c4bfa688d0fa86a09710bdf9ffe
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:41:51 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,13 +2,18 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int minOperations(int[] nums) {
+    public int canBeTypedWords(String text, String brokenLetters) {
         int count = 0;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] <= nums[i-1]){
-                count += nums[i-1] - nums[i] + 1;
-                nums[i] = nums[i-1] + 1;
+        String[] words = text.split(" ");
+        for(String word : words){
+            boolean flag = true;
+            for(int i = 0; i < brokenLetters.length(); i++){
+                if(word.contains(brokenLetters.charAt(i) + "")){
+                    flag = false;
+                    break;
+                }
             }
+            if(flag) count++;
         }
         return count;
 
