91b7862da74734ad4c36cf2ef0b5a5e071569aac
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:32:41 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,20 +2,13 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public boolean repeatedSubstringPattern(String s) {
-        int n = s.length();
-        for(int i = n / 2; i >= 1; i--){
-            if(n % i == 0){
-                int m = n / i;
-                String sub = s.substring(0, i);
-                StringBuilder sb = new StringBuilder();
-                for(int j = 0; j < m; j++){
-                    sb.append(sub);
-                }
-                if(sb.toString().equals(s)) return true;
-            }
-        }
-        return false;
-        
+    public int[] constructRectangle(int area) {
+        int[] ans = new int[2];
+        int w = (int)Math.sqrt(area);
+        while(area % w != 0) w--;
+        ans[0] = area / w;
+        ans[1] = w;
+        return ans;
+
     }
 }
