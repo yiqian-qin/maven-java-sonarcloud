6d9951f64d15162ca413e6a8941aad30e882dd0e
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:16:41 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,20 +2,14 @@ import java.util.*;
 import java.util.Arrays;
 
 class Solution {
-    public String kthDistinct(String[] arr, int k) {
-        Map<String, Integer> countMap = new HashMap<>();
-        for (String s : arr) {
-            countMap.put(s, countMap.getOrDefault(s, 0) + 1);
-        }
-        int distinctCount = 0;
-        for (String s : arr) {
-            if (countMap.get(s) == 1) {
-                distinctCount++;
-                if (distinctCount == k) {
-                    return s;
-                }
+    public int minOperations(int[] nums) {
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i-1]) {
+                count += nums[i-1] - nums[i] + 1;
+                nums[i] = nums[i-1] + 1;
             }
         }
-        return "";
+        return count;
     }
 }
