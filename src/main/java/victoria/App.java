d28068f7455f098d9aeb78b37c2dd7ede600d3ec
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 18:02:11 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,28 +2,37 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int[] missingRolls(int[] rolls, int mean, int n) {
-        int m = rolls.length;
-        int sum = 0;
-        for (int i = 0; i < m; i++) {
-            sum += rolls[i];
+    public int calculate(String s) {
+        int n = s.length();
+        Stack<Integer> stack = new Stack<>();
+        int sign = 1;
+        int result = 0;
+        for(int i = 0; i < n; i++){
+            if(Character.isDigit(s.charAt(i))){
+                int num = s.charAt(i) - '0';
+                while(i + 1 < n && Character.isDigit(s.charAt(i + 1))){
+                    num = num * 10 + (s.charAt(i + 1) - '0');
+                    i++;
+                }
+                result += sign * num;
+            }
+            else if(s.charAt(i) == '+'){
+                sign = 1;
+            }
+            else if(s.charAt(i) == '-'){
+                sign = -1;
+            }
+            else if(s.charAt(i) == '('){
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            }
+            else if(s.charAt(i) == ')'){
+                result = result * stack.pop() + stack.pop();
+            }
         }
-        int total = (m + n) * mean;
-        int missing = total - sum;
-        if (missing < n || missing > 6 * n) {
-            return new int[0];
-        }
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            res[i] = 1;
-        }
-        missing -= n;
-        for (int i = n - 1; i >= 0; i--) {
-            int add = Math.min(5, missing);
-            res[i] += add;
-            missing -= add;
-        }
-        return res;
+        return result;
         
     }
 }
