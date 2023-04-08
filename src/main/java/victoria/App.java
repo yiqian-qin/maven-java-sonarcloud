02a8327e7061159b19671dfe16b217bd4f769583
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 18:05:47 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,26 +2,18 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        int n = profits.length;
-        int[][] arr = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = capital[i];
-            arr[i][1] = profits[i];
-        }
-        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        int i = 0;
-        while (k-- > 0) {
-            while (i < n && arr[i][0] <= w) {
-                pq.offer(arr[i++][1]);
-            }
-            if (!pq.isEmpty()) {
-                w += pq.poll();
-            } else {
-                break;
+        int time = 0;
+        for (int[] c : courses) {
+            time += c[0];
+            pq.offer(c[0]);
+            if (time > c[1]) {
+                time -= pq.poll();
             }
         }
-        return w;
+        return pq.size();
+        
     }
 }
