ceef40f2d657a4fcc8e786c2d5e17ecad64f985c
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:55:27 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,20 +2,32 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int twoCitySchedCost(int[][] costs) {
-        int res = 0;
-        int n = costs.length / 2;
-        int[] diff = new int[n * 2];
-        for (int i = 0; i < n * 2; i++) {
-            diff[i] = costs[i][0] - costs[i][1];
-            res += costs[i][1];
+    public int maxLevelSum(TreeNode root) {
+        int maxSum = Integer.MIN_VALUE;
+        int maxLevel = 0;
+        int level = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxLevel = level;
+            }
+            level++;
         }
-        Arrays.sort(diff);
-        for (int i = 0; i < n; i++) {
-            res += diff[i];
-        }
-        return res;
-        
+        return maxLevel;
         
     }
 }
