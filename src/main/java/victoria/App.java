dae162d0b453be96d8d41797712ea78bfb722066
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 16:47:06 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,28 +2,21 @@ import java.util.*;
 import java.util.Arrays;
 
 class Solution {
-    public int minDeletionSize(String[] strs) {
-        int n = strs.length;
-        int m = strs[0].length();
-        int[] dp = new int[m]; // to store longest common subsequence
-        Arrays.fill(dp, 1); // intialize the array with 1s
-        
-        for(int j=1; j<m; j++) {
-            // compare each column with previous column
-            for(int i=0; i<j; i++) {
-                boolean isSorted = true;
-                // compare each row
-                for(int k=0; k<n; k++) {
-                    if(strs[k].charAt(i) > strs[k].charAt(j)) {
-                        isSorted = false;
-                        break;
-                    }
-                }
-                if(isSorted) {
-                    dp[j] = Math.max(dp[j], dp[i]+1);
-                }
-            }
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        List<Interval> merged = new ArrayList<>();
+        for (List<Interval> intervals : schedule) {
+            merged.addAll(intervals);
         }
-        return m - Arrays.stream(dp).max().getAsInt(); // return the columns that are not part of longest common subsequence
+        Collections.sort(merged, (a, b) -> a.start - b.start);
+
+        List<Interval> result = new ArrayList<>();
+        int end = merged.get(0).end;
+        for (Interval interval : merged) {
+            if (interval.start > end) {
+                result.add(new Interval(end, interval.start));
+            }
+            end = Math.max(end, interval.end);
+        }
+        return result;
     }
 }
