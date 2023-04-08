19187b68272a3556480f448850c44cc1106d7bdf
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:37:10 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,18 +2,14 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int findLengthOfLCIS(int[] nums) {
-        if(nums.length == 0) return 0;
-        int max = 1;
-        int count = 1;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] > nums[i-1]){
-                count++;
-            }else{
-                count = 1;
-            }
-            max = Math.max(max, count);
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length+1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2; i < dp.length; i++){
+            dp[i] = Math.min(dp[i-1], dp[i-2]) + (i == dp.length-1 ? 0 : cost[i]);
         }
-        return max;
+        return dp[dp.length-1];
+
     }
 }
