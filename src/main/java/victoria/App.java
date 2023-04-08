6243275e5b5396150510c6df9716b6a525e688de
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:01:33 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -1,65 +1,15 @@
 import java.util.*;
 import java.util.Arrays;
 
-class SummaryRanges {
-
-    List<int[]> intervals; // List to store intervals
-
-    public SummaryRanges() {
-        intervals = new ArrayList<>();
-    }
-
-    public void addNum(int val) {
-        int n = intervals.size();
-        if (n == 0) { //if interval list is empty
-            intervals.add(new int[]{val, val});
-            return;
+class Solution {
+    public int countDigitOne(int n) {
+        int count = 0;
+        long i = 1;
+        while (i <= n) {
+            long divider = i * 10;
+            count += (n / divider) * i + Math.min(Math.max(n % divider - i + 1, 0L), i);
+            i *= 10;
         }
-        // check if val is lesser than first start in interval list
-        if (intervals.get(0)[0] > val) {
-            if (intervals.get(0)[0] == val + 1) {
-                intervals.get(0)[0] = val;
-            } else {
-                intervals.add(0, new int[]{val, val});
-            }
-            return;
-        }
-        // check if val lies in a middle interval
-        for (int i = 0; i < n-1;i++) {
-            int start = intervals.get(i)[0];
-            int end = intervals.get(i)[1];
-            if (start <= val && end >= val) return; //if val is already in an interval
-            int nextStart = intervals.get(i+1)[0];
-            if (end < val && nextStart > val) {
-                if (end + 1 == val && nextStart - 1 == val) {
-                    intervals.get(i)[1] = intervals.remove(i+1)[1];
-                } else if (end + 1 == val) {
-                    intervals.get(i)[1] = val;
-                } else if (nextStart - 1 == val) {
-                    intervals.get(i+1)[0] = val;
-                } else {
-                    intervals.add(i+1, new int[]{val, val});
-                }
-                return;
-            }
-        }
-        int[] last = intervals.get(n-1);
-        // check if val is greater than last end in interval list
-        if (last[1] < val) {
-            if (last[1] == val - 1) {
-                last[1] = val;
-            } else {
-                intervals.add(new int[]{val, val});
-            }
-            return;
-        }
-    }
-
-    public int[][] getIntervals() {
-        int[][] res = new int[intervals.size()][2];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = intervals.get(i);
-        }
-        return res;
+        return count;
     }
 }
