1df2daa1da722f511fc1199d23965f88200bae4e
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:17:27 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,14 +2,15 @@ import java.util.*;
 import java.util.Arrays;
 
 class Solution {
-    public int minOperations(int[] nums) {
-        int count = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] <= nums[i-1]) {
-                count += nums[i-1] - nums[i] + 1;
-                nums[i] = nums[i-1] + 1;
-            }
-        }
-        return count;
+    int minDiff = Integer.MAX_VALUE; // initialize the minimum difference as maximum integer value
+    Integer prev = null; // initialize previous node value as null
+    
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) return minDiff; // if the BST is null, return the current minimum difference
+        getMinimumDifference(root.left); // traverse left subtree
+        if (prev != null) minDiff = Math.min(minDiff, root.val - prev); // check and update the minimum difference
+        prev = root.val; // set previous node value as current node value
+        getMinimumDifference(root.right); // traverse right subtree
+        return minDiff; // return the final minimum difference
     }
 }
