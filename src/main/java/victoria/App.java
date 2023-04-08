646bb415a772d137bd96817b3fe0a611073f0c01
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:40:09 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,29 +2,14 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public boolean isCousins(TreeNode root, int x, int y) {
-        if(root == null) return false;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            boolean xFound = false;
-            boolean yFound = false;
-            for(int i = 0; i < size; i++){
-                TreeNode node = queue.poll();
-                if(node.val == x) xFound = true;
-                if(node.val == y) yFound = true;
-                if(node.left != null && node.right != null){
-                    if(node.left.val == x && node.right.val == y) return false;
-                    if(node.left.val == y && node.right.val == x) return false;
-                }
-                if(node.left != null) queue.add(node.left);
-                if(node.right != null) queue.add(node.right);
-            }
-            if(xFound && yFound) return true;
-            if(xFound || yFound) return false;
+    public int minStartValue(int[] nums) {
+        int min = 0;
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            min = Math.min(min, sum);
         }
-        return false;
+        return 1 - min;
 
     }
 }
