ba921917d709da3df49c5560806eac3ba0a68cdb
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:58:09 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,22 +2,51 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int[] findBuildings(int[] heights) {
-        int n = heights.length;
-        int[] res = new int[n];
-        int max = 0;
-        int index = n - 1;
-        for (int i = n - 1; i >= 0; i--) {
-            if (heights[i] > max) {
-                res[index--] = i;
-                max = heights[i];
+    public int minSwaps(String s) {
+        int n = s.length();
+        int count0 = 0;
+        int count1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '0') {
+                count0++;
+            } else {
+                count1++;
             }
         }
-        int[] ans = new int[n - index - 1];
-        for (int i = 0; i < ans.length; i++) {
-            ans[i] = res[index + 1 + i];
+        if (Math.abs(count0 - count1) > 1) {
+            return -1;
         }
-        return ans;
+        if (count0 > count1) {
+            int res = 0;
+            for (int i = 0; i < n; i += 2) {
+                if (s.charAt(i) == '1') {
+                    res++;
+                }
+            }
+            return res;
+        } else if (count0 < count1) {
+            int res = 0;
+            for (int i = 0; i < n; i += 2) {
+                if (s.charAt(i) == '0') {
+                    res++;
+                }
+            }
+            return res;
+        } else {
+            int res1 = 0;
+            int res2 = 0;
+            for (int i = 0; i < n; i += 2) {
+                if (s.charAt(i) == '1') {
+                    res1++;
+                }
+            }
+            for (int i = 0; i < n; i += 2) {
+                if (s.charAt(i) == '0') {
+                    res2++;
+                }
+            }
+            return Math.min(res1, res2);
+        }
         
     }
 }
