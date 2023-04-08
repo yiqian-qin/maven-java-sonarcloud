1f65f571487eb1187c35273f7aaeadb254a3c36a
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:54:35 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,20 +2,20 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int maxSubarraySumCircular(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        int sum = 0;
-        int curMax = 0;
-        int curMin = 0;
-        for (int num : nums) {
-            curMax = Math.max(curMax + num, num);
-            max = Math.max(max, curMax);
-            curMin = Math.min(curMin + num, num);
-            min = Math.min(min, curMin);
-            sum += num;
+    public int twoCitySchedCost(int[][] costs) {
+        int res = 0;
+        int n = costs.length / 2;
+        int[] diff = new int[n * 2];
+        for (int i = 0; i < n * 2; i++) {
+            diff[i] = costs[i][0] - costs[i][1];
+            res += costs[i][1];
         }
-        return max > 0 ? Math.max(max, sum - min) : max;
+        Arrays.sort(diff);
+        for (int i = 0; i < n; i++) {
+            res += diff[i];
+        }
+        return res;
+        
         
     }
 }
