ef4632055326a4c07b2c711a5604d5c42b54dfbb
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:43:51 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,32 +2,15 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int[] sortEvenOdd(int[] nums) {
-        int[] odd = new int[nums.length];
-        int[] even = new int[nums.length];
-        int oddCount = 0;
-        int evenCount = 0;
+    public int findClosestNumber(int[] nums) {
+        int result = 0;
+        int min = Integer.MAX_VALUE;
         for(int i = 0; i < nums.length; i++){
-            if(i % 2 == 0){
-                even[evenCount] = nums[i];
-                evenCount++;
-            }else{
-                odd[oddCount] = nums[i];
-                oddCount++;
-            }
-        }
-        Arrays.sort(odd, 0, oddCount);
-        Arrays.sort(even, 0, evenCount);
-        int[] result = new int[nums.length];
-        int oddIndex = oddCount - 1;
-        int evenIndex = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(i % 2 == 0){
-                result[i] = even[evenIndex];
-                evenIndex++;
-            }else{
-                result[i] = odd[oddIndex];
-                oddIndex--;
+            if(Math.abs(nums[i]) < min){
+                min = Math.abs(nums[i]);
+                result = nums[i];
+            }else if(Math.abs(nums[i]) == min){
+                result = Math.max(result, nums[i]);
             }
         }
         return result;
