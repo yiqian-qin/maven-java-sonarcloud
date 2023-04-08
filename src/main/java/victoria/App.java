89066a2371b818fac8d5fc0ad63bfad176ca8ec2
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 16:50:47 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,24 +2,22 @@ import java.util.*;
 import java.util.Arrays;
 
 class Solution {
-    public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses, (a,b) -> a[1] - b[1]); // sort courses by last day
-
-        PriorityQueue<Integer> takenCourses = new PriorityQueue<>((a, b) -> b - a); // max heap to keep track of taken courses
-        int currentDay = 0; // current day we start a course
-
-        for (int[] course : courses) {
-            int duration = course[0], lastDay = course[1];
-            if (currentDay + duration <= lastDay) {
-                takenCourses.offer(duration);
-                currentDay += duration;
-            } else if (!takenCourses.isEmpty() && takenCourses.peek() > duration) {
-                // replace a longer course with this one to reduce total days taken
-                currentDay += duration - takenCourses.poll();
-                takenCourses.offer(duration);
+    public int removeBoxes(int[] boxes) {
+        int n = boxes.length;
+        int[][][] dp = new int[n][n][n];
+        return dfs(dp, boxes, 0, n - 1, 0);
+    }
+    
+    private int dfs(int[][][] dp, int[] boxes, int i, int j, int k) {
+        if (i > j) return 0;
+        if (dp[i][j][k] > 0) return dp[i][j][k];
+        int res = (k + 1) * (k + 1) + dfs(dp, boxes, i + 1, j, 0);
+        for (int m = i + 1; m <= j; ++m) {
+            if (boxes[m] == boxes[i]) {
+                res = Math.max(res, dfs(dp, boxes, i + 1, m - 1, 0) + dfs(dp, boxes, m, j, k + 1));
             }
         }
-
-        return takenCourses.size();
+        dp[i][j][k] = res;
+        return res;
     }
 }
