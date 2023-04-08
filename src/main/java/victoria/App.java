a3bea0d41c87c7faf80066eea7bc41b822ad7f83
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:50:40 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,21 +2,22 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int left = 0;
-        int right = nums.length - 1;
+        int right = arr.length - k;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (mid % 2 == 1) {
-                mid--;
-            }
-            if (nums[mid] == nums[mid + 1]) {
-                left = mid + 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
             } else {
                 right = mid;
             }
         }
-        return nums[left];
+        List<Integer> res = new ArrayList<>();
+        for (int i = left; i < left + k; i++) {
+            res.add(arr[i]);
+        }
+        return res;
         
     }
 }
