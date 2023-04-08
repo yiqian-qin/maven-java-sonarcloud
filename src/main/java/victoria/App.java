30e0b7294eddb314bb1070fb885ceebf0e18e930
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:48:30 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,22 +2,29 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public double myPow(double x, int n) {
-        if(n == 0){
-            return 1;
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currString = new StringBuilder();
+        int currCount = 0;
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)){
+                currCount = currCount * 10 + c - '0';
+            } else if(c == '['){
+                countStack.push(currCount);
+                stringStack.push(currString);
+                currString = new StringBuilder();
+                currCount = 0;
+            } else if(c == ']'){
+                StringBuilder temp = currString;
+                currString = stringStack.pop();
+                for(int i = countStack.pop(); i > 0; i--){
+                    currString.append(temp);
+                }
+            } else {
+                currString.append(c);
+            }
         }
-        if(n == 1){
-            return x;
-        }
-        if(n == -1){
-            return 1 / x;
-        }
-        double half = myPow(x, n / 2);
-        if(n % 2 == 0){
-            return half * half;
-        }else{
-            return half * half * myPow(x, n % 2);
-        }
-
+        return currString.toString();
     }
 }
