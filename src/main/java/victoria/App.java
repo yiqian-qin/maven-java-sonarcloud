80db3a09135e30f3602ef51761c35d31370866aa
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 16:54:43 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,22 +2,27 @@ import java.util.*;
 import java.util.Arrays;
 
 class Solution {
-    public int removeBoxes(int[] boxes) {
-        int n = boxes.length;
-        int[][][] dp = new int[n][n][n];
-        return dfs(dp, boxes, 0, n - 1, 0);
-    }
-    
-    private int dfs(int[][][] dp, int[] boxes, int i, int j, int k) {
-        if (i > j) return 0;
-        if (dp[i][j][k] > 0) return dp[i][j][k];
-        int res = (k + 1) * (k + 1) + dfs(dp, boxes, i + 1, j, 0);
-        for (int m = i + 1; m <= j; ++m) {
-            if (boxes[m] == boxes[i]) {
-                res = Math.max(res, dfs(dp, boxes, i + 1, m - 1, 0) + dfs(dp, boxes, m, j, k + 1));
-            }
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+        int[][] projects = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            projects[i][0] = capital[i];
+            projects[i][1] = profits[i];
         }
-        dp[i][j][k] = res;
-        return res;
+        Arrays.sort(projects, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        int i = 0;
+        while (k > 0) {
+            while (i < n && projects[i][0] <= w) {
+                pq.offer(projects[i][1]);
+                i++;
+            }
+            if (pq.isEmpty()) {
+                break;
+            }
+            w += pq.poll();
+            k--;
+        }
+        return w;
     }
 }
