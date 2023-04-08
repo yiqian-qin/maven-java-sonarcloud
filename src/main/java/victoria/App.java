b9c350d8f391b68f5abd700538b8ad0cd6ba6f2c
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 18:03:37 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,37 +2,13 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int calculate(String s) {
-        int n = s.length();
-        Stack<Integer> stack = new Stack<>();
-        int sign = 1;
-        int result = 0;
-        for(int i = 0; i < n; i++){
-            if(Character.isDigit(s.charAt(i))){
-                int num = s.charAt(i) - '0';
-                while(i + 1 < n && Character.isDigit(s.charAt(i + 1))){
-                    num = num * 10 + (s.charAt(i + 1) - '0');
-                    i++;
-                }
-                result += sign * num;
-            }
-            else if(s.charAt(i) == '+'){
-                sign = 1;
-            }
-            else if(s.charAt(i) == '-'){
-                sign = -1;
-            }
-            else if(s.charAt(i) == '('){
-                stack.push(result);
-                stack.push(sign);
-                result = 0;
-                sign = 1;
-            }
-            else if(s.charAt(i) == ')'){
-                result = result * stack.pop() + stack.pop();
-            }
+    public int countDigitOne(int n) {
+        int count = 0;
+        for(long i = 1; i <= n; i *= 10){
+            long divider = i * 10;
+            count += (n / divider) * i + Math.min(Math.max(n % divider - i + 1, 0), i);
         }
-        return result;
+        return count;
         
     }
 }
