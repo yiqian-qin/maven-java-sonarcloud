a5b34d9e8ecdaee8c3e09f87f27ce974ec7b4cd5
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:56:10 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,32 +2,22 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public int maxLevelSum(TreeNode root) {
-        int maxSum = Integer.MIN_VALUE;
-        int maxLevel = 0;
-        int level = 1;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            int sum = 0;
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                sum += node.val;
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
+    public int[] findBuildings(int[] heights) {
+        int n = heights.length;
+        int[] res = new int[n];
+        int max = 0;
+        int index = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (heights[i] > max) {
+                res[index--] = i;
+                max = heights[i];
             }
-            if (sum > maxSum) {
-                maxSum = sum;
-                maxLevel = level;
-            }
-            level++;
         }
-        return maxLevel;
+        int[] ans = new int[n - index - 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = res[index + 1 + i];
+        }
+        return ans;
         
     }
 }
