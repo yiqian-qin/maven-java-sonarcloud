177154a5609146e6e9e4a2cec827059a08fa3b2c
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 17:23:57 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -1,43 +1,21 @@
 import java.util.*;
 import java.util.Arrays;
-
-class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode() {}
-     TreeNode(int val) { this.val = val; }
-     TreeNode(int val, TreeNode left, TreeNode right) {
-         this.val = val;
-         this.left = left;
-         this.right = right;
-     }
- }
  
 class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
-        // Call the helper function
-        return constructBST(nums, 0, nums.length - 1);
-    }
-    
-    // Helper function to construct the BST recursively
-    private TreeNode constructBST(int[] nums, int left, int right) {
-        // Base case: returned when the left index is greater than the right index
-        if (left > right) {
-            return null;
-        }
-        
-        // Calculate the mid index
-        int mid = (left + right) / 2;
-        
-        // Construct a new TreeNode with val = the value of the mid index
-        TreeNode current = new TreeNode(nums[mid]);
-        
-        // Recursively call the helper function to construct the left and right subtrees respectively
-        current.left = constructBST(nums, left, mid - 1);
-        current.right = constructBST(nums, mid + 1, right);
-        
-        // Return the constructed TreeNode
-        return current;
+    public int countCornerRectangles(int[][] grid) {
+         int count = 0;
+         int m = grid.length, n = grid[0].length;
+         for (int i = 0; i < m - 1; i++) {
+             for (int j = i + 1; j < m; j++) {
+                 int currCount = 0;
+                 for (int k = 0; k < n; k++) {
+                     if (grid[i][k] == 1 && grid[j][k] == 1) {
+                         currCount++;
+                     }
+                 }
+                 count += currCount * (currCount - 1) / 2;
+             }
+         }
+         return count;
     }
 }
