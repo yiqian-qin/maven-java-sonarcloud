0439463605723f6c34f4bb96d66433f0f4948bfc
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-08 16:59:12 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -1,117 +1,65 @@
 import java.util.*;
 import java.util.Arrays;
 
-class LFUCache {
+class SummaryRanges {
 
-    // inner class to represent a node in Doubly LinkedList
-    class Node {
-        int key;
-        int value;
-        int freq;  // frequency of the key
-        Node prev;
-        Node next;
+    List<int[]> intervals; // List to store intervals
 
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-            this.freq = 1;
-        }
+    public SummaryRanges() {
+        intervals = new ArrayList<>();
     }
 
-    // inner class to represent a Doubly LinkedList
-    class DoublyLinkedList {
-        Node head;
-        Node tail;
-        int size;
-
-        public DoublyLinkedList() {
-            head = new Node(-1, -1);
-            tail = new Node(-1, -1);
-            head.next = tail;
-            tail.prev = head;
-            size = 0;
-        }
-
-        // add a new node at the beginning of the list
-        public void addFirst(Node node) {
-            node.next = head.next;
-            node.prev = head;
-            head.next.prev = node;
-            head.next = node;
-            size++;
-        }
-
-        // remove a node from the list
-        public void remove(Node node) {
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-            size--;
-        }
-
-        // remove the last node from the list
-        public Node removeLast() {
-            if (tail.prev == head) {
-                return null;
-            }
-            Node last = tail.prev;
-            remove(last);
-            return last;
-        }
-    }
-
-    Map<Integer, Node> cache;  // to store nodes corresponding to a key
-    Map<Integer, DoublyLinkedList> freqMap;  // to store Doubly LinkedLists corresponding to a frequency
-    int capacity;
-    int minFreq;  // minimum frequency in the cache
-
-    public LFUCache(int capacity) {
-        cache = new HashMap<>();
-        freqMap = new HashMap<>();
-        this.capacity = capacity;
-        this.minFreq = 0;
-    }
-
-    public int get(int key) {
-        if (!cache.containsKey(key)) {  // key not found
-            return -1;
-        }
-        Node node = cache.get(key);
-        update(node);  // update the frequency of the node
-        return node.value;
-    }
-
-    public void put(int key, int value) {
-        if (capacity == 0) {  // edge case
+    public void addNum(int val) {
+        int n = intervals.size();
+        if (n == 0) { //if interval list is empty
+            intervals.add(new int[]{val, val});
             return;
         }
-        if (cache.containsKey(key)) {  // key already exists in the cache
-            Node node = cache.get(key);
-            node.value = value;  // update the value of the existing node
-            update(node);  // update the frequency of the node
-        } else {  // key does not exist in the cache
-            if (cache.size() == capacity) {  // cache is full, need to evict a node
-                DoublyLinkedList dll = freqMap.get(minFreq);  // get the Doubly LinkedList corresponding to minimum frequency
-                Node evictedNode = dll.removeLast();  // remove the least frequently used node from the Doubly LinkedList
-                cache.remove(evictedNode.key);  // remove the evicted node from the cache
+        // check if val is lesser than first start in interval list
+        if (intervals.get(0)[0] > val) {
+            if (intervals.get(0)[0] == val + 1) {
+                intervals.get(0)[0] = val;
+            } else {
+                intervals.add(0, new int[]{val, val});
             }
-            Node newNode = new Node(key, value);
-            cache.put(key, newNode);  // add the new node to the cache
-            DoublyLinkedList dll = freqMap.computeIfAbsent(1, k -> new DoublyLinkedList());  // get the Doubly LinkedList corresponding to frequency 1 or create a new one if it does not exist
-            dll.addFirst(newNode);  // add the new node to the Doubly LinkedList corresponding to frequency 1
-            minFreq = 1;  // reset the minFreq to 1 after adding a new node
+            return;
+        }
+        // check if val lies in a middle interval
+        for (int i = 0; i < n-1;i++) {
+            int start = intervals.get(i)[0];
+            int end = intervals.get(i)[1];
+            if (start <= val && end >= val) return; //if val is already in an interval
+            int nextStart = intervals.get(i+1)[0];
+            if (end < val && nextStart > val) {
+                if (end + 1 == val && nextStart - 1 == val) {
+                    intervals.get(i)[1] = intervals.remove(i+1)[1];
+                } else if (end + 1 == val) {
+                    intervals.get(i)[1] = val;
+                } else if (nextStart - 1 == val) {
+                    intervals.get(i+1)[0] = val;
+                } else {
+                    intervals.add(i+1, new int[]{val, val});
+                }
+                return;
+            }
+        }
+        int[] last = intervals.get(n-1);
+        // check if val is greater than last end in interval list
+        if (last[1] < val) {
+            if (last[1] == val - 1) {
+                last[1] = val;
+            } else {
+                intervals.add(new int[]{val, val});
+            }
+            return;
         }
     }
 
-    // utility function to update the frequency of a node
-    private void update(Node node) {
-        int freq = node.freq;
-        DoublyLinkedList dll = freqMap.get(freq);  // get the Doubly LinkedList corresponding to the frequency of the node
-        dll.remove(node);  // remove the node from its current Doubly LinkedList
-        if (freq == minFreq && dll.size == 0) {  // update minFreq if necessary
-            minFreq++;
+    public int[][] getIntervals() {
+        int[][] res = new int[intervals.size()][2];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = intervals.get(i);
         }
-        node.freq++;  // increment the frequency of the node
-        DoublyLinkedList newDll = freqMap.computeIfAbsent(node.freq, k -> new DoublyLinkedList());  // get the Doubly LinkedList corresponding to the new frequency or create a new one if it does not exist
-        newDll.addFirst(node);  // add the node to the Doubly LinkedList corresponding to its new frequency
+        return res;
     }
 }
