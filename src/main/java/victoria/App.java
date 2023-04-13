87d11e1ba19b6075858eb43ce35739fa6ab96b00
yiqian-qin <113483001+yiqian-qin@users.noreply.github.com>
2023-04-12 21:31:23 -0400

    Update App.java

--- src/main/java/victoria/App.java
+++ src/main/java/victoria/App.java
@@ -2,24 +2,19 @@ import java.util.*;
 import java.util.Arrays;
  
 class Solution {
-    public double frogPosition(int n, int[][] edges, int t, int target) {
-        double[][] dp = new double[t+1][n+1];
-        dp[0][1] = 1.0; //initializing position of the frog at the beginning
-        
-        for(int i=1; i<=t; i++) {
-            for(int j=1; j<=n; j++) {
-                for(int[] edge: edges) {
-                    int u = edge[0], v = edge[1];
-                    if(u==j || v==j) { //checking if the current node is vertex of the current edge
-                        int neighbor = (u==j) ? v : u;
-                        if(dp[i-1][j] > 0) { //checking if there is already some probability of reaching the node
-                            dp[i][neighbor] += dp[i-1][j]/(edge.length-1); //adding probability to neighbor
-                        }
-                    }
-                }
-                if(i==t && j==target) return dp[t][target]; //returning the required probability for target node, if last second is reached and checking for target
-            }
+    public double myPow(double x, int n) {
+        if (n == 0) { // if the power is zero, return 1
+            return 1;
+        } 
+        if (n < 0) { // if the power is negative, calculate pow(x,-n) and return 1/pow(x,-n)
+            return 1 / myPow(x, -n);
         }
-        return 0.0; //if there is no probability of reaching the target node, then returning 0.0
+        double res = myPow(x, n/2); // calculating the result by dividing the problem into subproblems and using recursion
+        if (n % 2 == 0) { // if n is even, use the formula x^n = (x^(n/2))^2
+            res = res * res;
+        } else { // else use the formula x^n = x * (x^(n/2))^2
+            res = x * res * res;
+        }
+        return res;
     }
 }
